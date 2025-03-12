d31447af2621914b61611f4825fe5b84acb42b61
DavlatyarovShahzod <davlatyarovshahzod09@gmail.com>
2025-03-12 13:46:05 +0500

    birinchi yuklash

---
+++ src/components/search-panel/SearchPanel.vue
@@ -0,0 +1,35 @@
+<template>
+    <Input class="search-input" placeholder="Kinolarni qidirish" v-model="term" @input="changeHandler"/>
+</template>
+<script>
+import Input from '@/ui-components/Input.vue';
+
+export default {
+    props: {
+        updateTermHandler: {
+            type: Function,
+            required: true,
+        },
+    },
+    data() {
+        return {
+            term: '', 
+        }
+    },
+    methods: {
+        changeHandler(e) {
+            this.term = e.target.value,
+            this.updateTermHandler(this.term)
+        },
+    },
+}
+</script>
+<style scoped>
+    search-input{
+        padding: 1.5rem;
+        background-color: #fcfaf5;
+        margin-bottom: 1.5rem;
+        border-radius: 4px;
+        box-shadow: 15px 15px 15px rgba(0, 0, 0, 0.15);
+    }
+</style>
